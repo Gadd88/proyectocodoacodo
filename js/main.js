bfaad217202a4378580e0b375b45708de43db36d
Gadd88 <matiassaade@gmail.com>
2022-10-15 14:28:17 -0300

    agregado api de clima y probando estilos

--- js/main.js
+++ js/main.js
@@ -1,3 +1,38 @@
+//CLIMA
+const apiKey = '3653d5bce8c0ea0f4386f3db4f6f79ac';
+const fetchData = position => {
+    const { latitude, longitude } = position.coords;
+    fetch(`http://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&appid=${apiKey}`)
+        .then(response => response.json())
+        .then(data => setWeatherData(data))
+}
+
+const setWeatherData = data =>{
+    console.log(data);
+    const weatherData = {
+        temp: data.main.temp,
+        location: data.name,
+        description: data.weather[0].main,
+    }
+    Object.keys(weatherData).forEach( key => {
+        document.getElementById(key).textContent = weatherData[key];
+    });
+}
+
+const onLoad = () => {
+    navigator.geolocation.getCurrentPosition(fetchData);
+}
+
+
+function mostrar(){
+    document.getElementById("cafe_img").innerHTML = `
+    <img src=${gontranCherrier}>
+    `
+    console.log(gontranCherrier)
+}
+
+
+
 document.getElementById("fotos").innerHTML = `
 <div class="card">
     <img src=${feca}>
