120992a384ab0cf033ecb377f4206af68276b9b7
Gadd88 <matiassaade@gmail.com>
2022-10-17 00:38:41 -0300

    agregadas cards generales

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ const apiKey = "3653d5bce8c0ea0f4386f3db4f6f79ac";
 const fetchData = (position) => {
   const { latitude, longitude } = position.coords;
   fetch(
-    `https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&appid=${apiKey}`
+    `https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&lang=sp,es&appid=${apiKey}`
   )
     .then((response) => response.json())
     .then((data) => setWeatherData(data));
