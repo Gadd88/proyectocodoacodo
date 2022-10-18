956dd20d42ddcdfc2cdbec7dd1666609eb152880
Gadd88 <matiassaade@gmail.com>
2022-10-18 01:29:23 -0300

    cambiados algunos estilos

--- js/lugares.js
+++ js/lugares.js
@@ -1,17 +1,34 @@
 //VENTANA MODAL
 
-const open = document.getElementById('open1');
+const open = document.querySelectorAll('.open_modal');
 const modal_ventana = document.getElementById('modal_ventana');
 const close = document.getElementById('close_modal');
+const modal_contenedor = document.getElementsByClassName('modal_contenedor')[0];
 
-open.addEventListener('click', () => {
-  modal_ventana.style.opacity = 1
-  modal_ventana.style.zIndex = 10;
-});
+
+
+for(const elem of open){
+  elem.addEventListener('click', () => {
+    modal_ventana.style.opacity = 1
+    modal_ventana.style.zIndex = 10;
+    modal_contenedor.style.zIndex = 12;
+    close.style.opacity = 1;
+  });
+}
+
+
+
+
+
 
 close.addEventListener('click', () => {
   modal_ventana.style.opacity = 0
   modal_ventana.style.zIndex = -1;
+  modal_contenedor.style.zIndex = -1;
+  close.style.opacity = 0;
 });
 
-document.getElementById('modal_ventana').innerHTML = `<img src=${gontranCherrier}`
+document.getElementById('modal_ventana').innerHTML = `<img src=${gontranCherrier}>`
+
+document.getElementById('modal_ventana').classList.add('carrusel-item');
+
--- js/main.js
+++ js/main.js
@@ -1,32 +1,32 @@
 //API CLIMA
 /*************************************************************************** */
 
-const onLoad = () => {
-  navigator.geolocation.getCurrentPosition(fetchData);
-};
+// const onLoad = () => {
+//   navigator.geolocation.getCurrentPosition(fetchData);
+// };
 
-const apiKey = "3653d5bce8c0ea0f4386f3db4f6f79ac";
+// const apiKey = "3653d5bce8c0ea0f4386f3db4f6f79ac";
 
-const fetchData = (position) => {
-  const { latitude, longitude } = position.coords;
-  fetch(
-    `https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&lang=sp,es&appid=${apiKey}`
-  )
-    .then((response) => response.json())
-    .then((data) => setWeatherData(data));
-};
+// const fetchData = (position) => {
+//   const { latitude, longitude } = position.coords;
+//   fetch(
+//     `https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&lang=sp,es&appid=${apiKey}`
+//   )
+//     .then((response) => response.json())
+//     .then((data) => setWeatherData(data));
+// };
 
-const setWeatherData = (data) => {
-  // console.log(data);
-  const weatherData = {
-    location: data.name,
-    temp: data.main.temp,
-    description: data.weather[0].main,
-  };
-  Object.keys(weatherData).forEach((key) => {
-    document.getElementById(key).textContent = `${weatherData[key]}`;
-  });
-};
+// const setWeatherData = (data) => {
+//   // console.log(data);
+//   const weatherData = {
+//     location: data.name,
+//     temp: data.main.temp,
+//     description: data.weather[0].main,
+//   };
+//   Object.keys(weatherData).forEach((key) => {
+//     document.getElementById(key).textContent = `${weatherData[key]}`;
+//   });
+// };
 
 /************************************************************************************ */
 
